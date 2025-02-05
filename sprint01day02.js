9eb08a2c638b0802c6179f26f15ed1014c5cec05
Pelin Özsezer <pelinozsezer@gmail.com>
2025-02-05 21:51:24 +0300

    no message

--- sprint01day02.js
+++ sprint01day02.js
@@ -1,4 +1,49 @@
 function tasKagitMakas(gamer) {
+  // Bilgisayarın rastgele seçimi
+  let computer = ['taş', 'kağıt', 'makas'][Math.floor(Math.random() * 3)];
+
+  // Oyun sonucunu belirleme
+  if (computer === gamer) {
+    return (
+      'Senin seçimin: ' +
+      gamer +
+      '. Bilgisayarın seçimi: ' +
+      computer +
+      '. Beraberlik!'
+    );
+  } else if (
+    (gamer === 'taş' && computer === 'makas') ||
+    (gamer === 'kağıt' && computer === 'taş') ||
+    (gamer === 'makas' && computer === 'kağıt')
+  ) {
+    return (
+      'Senin seçimin: ' +
+      gamer +
+      '. Bilgisayarın seçimi: ' +
+      computer +
+      '. Kazandın!'
+    );
+  } else {
+    return (
+      'Senin seçimin: ' +
+      gamer +
+      '. Bilgisayarın seçimi: ' +
+      computer +
+      '. Kaybettin!'
+    );
+  }
+}
+
+// const gamer = prompt('Seçiminizi yazınız: taş, kağıt, makas?').toLowerCase();
+// const gamer = ['taş', 'kağıt', 'makas'][Math.floor(Math.random() * 3)];
+// tasKagitMakas(gamer);
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = tasKagitMakas;
+
+////////// VERSİYON 2 //////////
+
+/* function tasKagitMakas(gamer) {
   const options = ['taş', 'kağıt', 'makas'];
   const computer = options[Math.floor(Math.random() * 3)];
 
@@ -10,7 +55,8 @@ function tasKagitMakas(gamer) {
     return `Senin seçimin: ${gamer}. Bilgisayarın seçimi: ${computer}. Beraberlik!`;
   }
 
-  const kazanmaKurallari = { // object= key: value...
+  const kazanmaKurallari = {
+    // object= key: value...
     taş: 'makas',
     kağıt: 'taş',
     makas: 'kağıt',
@@ -21,7 +67,7 @@ function tasKagitMakas(gamer) {
   } else {
     return `Senin seçimin: ${gamer}. Bilgisayarın seçimi: ${computer}. Kaybettin!`;
   }
-}
+} */
 
 // const gamer = prompt('Seçiminizi yazınız: taş, kağıt, makas?').toLowerCase(); tasKagitMakas(gamer);
 // const gamer = ['taş', 'kağıt', 'makas'][Math.floor(Math.random() * 3)]; tasKagitMakas(gamer);
